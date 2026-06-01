1b65497dbf49db4f1ab16a65bbff2aff060274c9
R Sashank Adithiyaa <sashankmidhun@gmail.com>
2026-06-01 20:33:12 +0400

    Update project.py

--- project.py
+++ project.py
@@ -49,7 +49,7 @@ elif option == "🗄️ MySQL Database":
     if st.sidebar.button("🔗 Connect to MySQL"):
         try:
             mydb = mysql.connector.connect(
-                host="localhost",   
+                host="your-cloud-host",   
                 user='root', 
                 password='code_RED', 
                 database='supply_chain',
